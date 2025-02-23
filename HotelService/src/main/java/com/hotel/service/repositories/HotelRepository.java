08a80807a7343aa161479e442facbba27bfed13d
Tejas Borse <tejasborse8114@gmail.com>
2025-02-24 00:38:04 +0530

    Delete HotelService directory

--- HotelService/src/main/java/com/hotel/service/repositories/HotelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hotel.service.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hotel.service.entites.Hotel;
-
-public interface HotelRepository extends JpaRepository<Hotel, String>{
-
-}
