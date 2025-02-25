2d7710774d735d33ac5c53de38075f101769e386
Tejas Borse <tejasborse8114@gmail.com>
2025-02-25 13:34:05 +0530

    just testing for commiter name

--- UserService/src/main/java/com/userservice/UserServiceApplication.java
+++ UserService/src/main/java/com/userservice/UserServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
